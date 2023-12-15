d2501e04639d9c1ef94b3951eda0743ca0fad43a
hayk <hayk.kerobyan@mait.io>
2023-12-15 12:36:26 -0300

    Comment added to SmartCaptureActivity

--- app/src/main/java/com/matilock/matiintegrationdemo/SmartCaptureActivity.java
+++ app/src/main/java/com/matilock/matiintegrationdemo/SmartCaptureActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.metamap.metamap_sdk.MetamapButton;
 import com.metamap.metamap_sdk.MetamapSdk;
 
+/**
+ * Once the smart capture dependency is added it will automatically be enabled.
+ * However, if you want to enable/disable it manually, please use the following methods
+ * Enable the Smart Capture: MetamapSdk.INSTANCE.enableSmartCapture()
+ * Disable the Smart Capture: MetamapSdk.INSTANCE.disableSmartCapture()
+ */
 public class SmartCaptureActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +20,6 @@ public class SmartCaptureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metamap_button);
 
-        MetamapSdk.INSTANCE.enableSmartCapture();
         MetamapButton btn = findViewById(R.id.metamapButton);
         btn.setParams(this, "YOUR_CLIENT_ID", "YOUR_FLOW_ID");
     }
